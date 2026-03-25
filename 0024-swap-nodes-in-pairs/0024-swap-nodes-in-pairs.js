a3601c2259d0343a7a9853b37022a1298e123388
Abhishek yadav <ydvabhishek447@gmail.com>
2026-03-25 10:50:08 +0530

    Time: 0 ms (100%), Space: 53.6 MB (71.66%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+
+    let dummy = new ListNode();
+
+    let prev = dummy;
+    let curr = head;
+    let next = head.next;
+
+    while(curr && next){
+        prev.next =  next;
+        curr.next = next.next;
+        next.next = curr;
+        prev = curr;
+        curr = prev.next;
+        next = curr && curr.next;
+
+    }
+
+    return dummy.next
+};
