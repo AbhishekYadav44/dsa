fa505c46f85a5f6a7c91de4892de161c9cf07c52
Abhishek yadav <ydvabhishek447@gmail.com>
2026-04-09 08:32:57 +0530

    Time: 0 ms (100%), Space: 54.2 MB (51.65%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.js
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -4,14 +4,18 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-   let set = new Set();
-   let count = 0;
-   for(let i = 0; i<jewels.length;i++){
-    set.add(jewels[i]);
-   } 
+    let map = new Set();
+    let count = 0;
 
-   for(let i= 0; i<stones.length;i++){
-    if(set.has(stones[i]))count++
-   }
-   return count;
+    for(let i = 0; i<jewels.length; i++){
+        map.add(jewels[i]);
+    }
+
+    for(let i =0; i<stones.length;i++){
+        if(map.has(stones[i])){
+           count++;
+        }
+    }
+
+    return count;
 };
